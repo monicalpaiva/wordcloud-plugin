7bc4ffeb97952d09036f83b4d8531da830dc784d
Monica Paiva <monicalpaiva@hotmail.com>
2019-06-07 17:00:01 -0300

    wordcloud

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @defgroup plugins_blocks_wordCloud wordCloud Block Plugin
+ */
+ 
+/**
+ * @file plugins/blocks/wordCloud/index.php
+ *
+ * Copyright (c) 2014-2018 Simon Fraser University
+ * Copyright (c) 2003-2018 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_blocks_wordCloud
+ * @brief Wrapper for wordCloud block plugin.
+ *
+ */
+
+require_once('wordCloudBlockPlugin.inc.php');
+
+return new wordCloudBlockPlugin();
+
+?> 
